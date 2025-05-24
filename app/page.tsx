ecb674069bafb8492b8e530c00b749f3095923e3
sachinuke36 <sachinuke36@gmail.com>
2025-05-24 14:05:53 +0530

    removed-unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
